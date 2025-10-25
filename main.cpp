39641d00a4213cacdf2d885e16bd27a02b258688
RapidMindd <YaroslavT007@yandex.ru>
2025-10-25 12:35:36 +0300

    Input one uint

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+
 int main()
 {
+  using uint = unsigned;
+  uint a = 0;
+  std::cin >> a;
+  
+  if (std::cin.eof()) {
+    std::cout << 0;
+    std::cout << "\n";
+  }
+  else if (std::cin.fail()) {
+    std::cout << "Error\n";
+    retutn 1;
+  }
 }
