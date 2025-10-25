0db404766662c1bd48d57b1c793a619aec026cc1
RapidMindd <YaroslavT007@yandex.ru>
2025-10-25 13:22:55 +0300

    first numbers input

--- main.cpp
+++ main.cpp
@@ -3,16 +3,17 @@
 bool isPyth(unsigned a, unsigned b, unsigned c)
 {
   unsigned a2 = a*a, b2 = b*b, c2 = c*c;
-  bool p = (a2 = b2 + 2c);
-  p = p || (b2 = a2 + c2);
-  p = p || (c2 = a2 + b2);
-  return c1 || c2 || c3;
+  bool p = (a2 = (b2 + c2));
+  p = p || (b2 = (a2 + c2));
+  p = p || (c2 = (a2 + b2));
+  return p;
 }
 
 int main()
 {
   using uint = unsigned;
   uint a = 0, b = 0, c = 0;
+  std::cin >> c >> b;
   size_t count = 0;
   while (std::cin >> a) {
     count += isPyth(a, b, c) ? 1 : 0;
