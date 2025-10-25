09078afc4195e88b6abccbb4376823095aeeb5a6
RapidMindd <YaroslavT007@yandex.ru>
2025-10-25 15:21:12 +0300

    squaring overflow added

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
+#include <limits>
 #include <iostream>
+
+const int MAX = std::numeric_limits<int>::max();
 
 bool isPyth(unsigned a, unsigned b, unsigned c)
 {
-  unsigned a2 = a*a, b2 = b*b, c2 = c*c;
-  bool p = (a2 == (b2 + c2));
-  p = p || (b2 == (a2 + c2));
-  p = p || (c2 == (a2 + b2));
-  return p;
+  if (!(MAX / a < a) && !(MAX / b < b) && !(MAX / c < c)) {
+    unsigned a2 = a*a, b2 = b*b, c2 = c*c;
+    bool p = (a2 == (b2 + c2));
+    p = p || (b2 == (a2 + c2));
+    p = p || (c2 == (a2 + b2));
+    return p;
+  }
+  else throw
+    std::overflow_error("sq_overflow");
 }
 
 int main()
@@ -16,16 +23,21 @@ int main()
   std::cin >> c >> b;
   size_t count = 0;
   while (std::cin >> a) {
-    count += isPyth(a, b, c) ? 1 : 0;
-    c = b;
-    b = a;
+    try {
+      count += isPyth(a, b, c) ? 1 : 0;
+      c = b;
+      b = a;
+    } catch (const std::overflow_error &) {
+      std::cerr << "Overflow while squaring\n";
+      return 2;
+    }
   }
   if (std::cin.eof()) {
     std::cout << count;
     std::cout << "\n";
   }
   else if (std::cin.fail()) {
-    std::cout << "Error\n";
+    std::cerr << "Error while reading\n";
     return 1;
   }
 }
