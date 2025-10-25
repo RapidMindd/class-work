982bfa1ed29e3508e5b81cd498f43812834d05c0
RapidMindd <YaroslavT007@yandex.ru>
2025-10-25 16:46:28 +0300

    division by zero fixed

--- main.cpp
+++ main.cpp
@@ -3,11 +3,25 @@
 
 const int MAX = std::numeric_limits<int>::max();
 
+bool sq_overflow(unsigned a, unsigned b, unsigned c) {
+    if ((a and MAX / a < a) || (b and MAX / b < b) || (c and MAX / c < c)) {
+        return 1;
+    }
+    return 0;
+}
+
+bool add_overflow(unsigned a, unsigned b, unsigned c) {
+    if ((MAX - a < b) || (MAX - a < c) || (MAX - b < c)) {
+        return 1;
+    }
+    return 0;
+}
+
 bool isPyth(unsigned a, unsigned b, unsigned c)
 {
-  if (!(MAX / a < a) && !(MAX / b < b) && !(MAX / c < c)) {
+  if (!sq_overflow(a, b, c)) {
     unsigned a2 = a*a, b2 = b*b, c2 = c*c;
-    if ((MAX - a2 > b2) && (MAX - a2 > c2) && (MAX - b2 > c2)) {
+    if (!add_overflow(a2, b2, c2)) {
       bool p = (a2 == (b2 + c2));
       p = p || (b2 == (a2 + c2));
       p = p || (c2 == (a2 + b2));
@@ -45,3 +59,4 @@ int main()
     return 1;
   }
 }
+
